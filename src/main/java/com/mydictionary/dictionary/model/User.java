9bc407323367fcbfd3f7024252d04ebe1253c65a
Vladimir <vova.prusenok@gmail.com>
2021-08-28 00:39:30 +0300

    remove redundant user_id from User.java

--- src/main/java/com/mydictionary/dictionary/model/User.java
+++ src/main/java/com/mydictionary/dictionary/model/User.java
@@ -2,24 +2,14 @@ package com.mydictionary.dictionary.model;
 
 public class User {
 
-    private int user_id;
     private String email;
     private String passw_hash;
 
     public User(){}
 
     public User(int user_id, String email, String passw_hash) {
-        this.user_id = user_id;
         this.email = email;
         this.passw_hash = passw_hash;
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
     }
 
     public String getEmail() {
